62f77e6cdc33244b7491f5fcff2af1e0e46ad212
rasya-ramesh <rasya.ramesh1999@gmail.com>
2020-01-27 09:54:42 +0530

    Integrated generation of parse tree. Run parse_tree-function.py. test.py is depracated and will be removed from repo

--- parse_tree-function.py
+++ parse_tree-function.py
@@ -7,22 +7,66 @@
 # Function grammar:
 # funcdef: 'def' NAME parameters  ':' func_body_suite
 # parameters: '(' [typedargslist] ')'
+from random import choice 
+
+class Node:
+  def __init__(self, n_type, value, children=None, leaf=None):
+    self.type = n_type
+    self.value = value
+    if children:
+      self.children = children
+    else:
+      self.children = [ ]
+
+    self.leaf = leaf
+  def __repr__(self, level=0):
+    ret = "\t"*level+repr(self.value)+"\n"
+    for child in self.children:
+      ret += child.__repr__(level+1)
+    return ret
+
 reserved = {
     'def' : 'DEF'
  }
 tokens = [ 'COLON', 'COMMA','TAB',  'NAME', 'LPAREN','RPAREN' ] + list(reserved.values())
 
 # Tokens
-t_COMMA   = r'\,'
-t_COLON   = r'\:'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-t_TAB     = r'\t'
+# t_COMMA   = r'\,'
+# t_COLON   = r'\:'
+# t_LPAREN  = r'\('
+# t_RPAREN  = r'\)'
+# t_TAB     = r'\t'
 #t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
+def t_COMMA(t):
+  r'\,'
+  t.value = Node("COMMA", ',', leaf = 1)
+  return t
+
+def t_COLON(t):
+  r'\:'
+  t.value = Node("COLON", ':', leaf = 1)
+  return t
+
+def t_LPAREN(t):
+  r'\('
+  t.value = Node("LPAREN", '(', leaf = 1)
+  return t
+
+def t_RPAREN(t):
+  r'\)'
+  t.value = Node("RPAREN", ')', leaf = 1)
+  return t
+
+def t_TAB(t):
+  r'\t'
+  t.value = Node("TAB", '\t', leaf = 1)
+  return t
+
 
 def t_NAME(t):
      r'[a-zA-Z_][a-zA-Z_0-9]*'
      t.type = reserved.get(t.value,'NAME')    # Check for reserved words
+     t.value = Node("NAME", t.value, leaf = 1)
      return t
 
 # Ignored characters
@@ -31,6 +75,7 @@ t_ignore = " "#"\t"
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count("\n")
+    # t.value = Node("NAME", t.value, leaf = 1)
 
 def p_empty(p):
      'empty :'
@@ -53,7 +98,7 @@ stmts=[]
 def p_funcdef(t):
     '''funcdef : DEF NAME LPAREN parameters RPAREN COLON funcbody'''
     print("function name:",t[2])
-    t[0]=(t[2],t[4],t[7])
+    t[0]=Node("function", "function", [t[1], t[2], t[3], t[4], t[5], t[6], t[7]], leaf = 0)
 
 def p_parameters(t):
     '''parameters : NAME
@@ -63,7 +108,7 @@ def p_parameters(t):
         print("arg: ",t[1])
         tlist.append(t[1])
 
-    t[0]=('args',tlist)
+    t[0]=Node("parameters", "parameters", tlist, leaf = 0)
 
 def p_funcbody(t):
     '''funcbody :  TAB statement
@@ -71,14 +116,14 @@ def p_funcbody(t):
     # print("BODY: " ," ".join(func_stmt))
     if(t[2]!=None):
         stmts.append(t[2])
-    t[0]=(stmts)
+    t[0]=Node("fbody", "fbody", stmts, leaf = 0)
 
 
 def p_statement(t):
     '''statement : NAME
                  | NAME statement'''            #have to change NAME with expressions/statements.
     # func_stmt.insert(0, t[1] )
-    t[0]=t[1]
+    t[0]=Node("stmts", "stmts", [t[1]], leaf = 0)
 
 def p_error(t):
     print("Syntax error at '%s'" % t.value)
@@ -97,8 +142,128 @@ yacc.yacc()
 #data = input('codesegment : \n')
 #data.replace('\n','')
 data='''def foo(a,b):
-\tsome
-\treturn a'''
+\n\tsome
+\n\treturn a'''
 #print(data)
 print("meh")
-print(yacc.parse(data))
+
+root = yacc.parse(data)
+
+
+def printYield(root, reqpos, type):
+    # Stack to store all the nodes  
+    # of tree  
+    s1 = []  
+  
+    # Stack to store all the 
+    # leaf nodes  
+    s2 = []  
+  
+    # Push the root node  
+    s1.append(root)  
+    n=0
+    while len(s1) != 0:  
+        curr = s1.pop()  
+  
+        # If current node has a left child  
+        # push it onto the first stack  
+        for child in curr.children:
+          s1.append(child)
+  
+        if curr.leaf:
+            n+=1
+            if type == "remove" and n not in reqpos:
+                s2.append(curr)
+
+            if type == "add":
+                s2.append(curr)
+                if n in reqpos:
+                    temp = Node("dummy", "errnode", leaf = 1)
+                    s2.append(temp)
+            if type == "replace":
+                if n in reqpos:
+                    temp = Node("dummy", "@@@", leaf = 1)
+                    s2.append(temp)
+                else:
+                    s2.append(curr)
+
+    # Print all the leaf nodes  
+    level = 0
+    while len(s2) != 0:
+      val = s2.pop()
+      print("\t"*level + val.value, end = " ")
+
+      colon = Node("COLON",":", leaf = 1)
+      if val == colon:
+        print("")
+        level += 1
+
+    print("\n------------------------------\n")
+
+def getPgmLen(root):
+    s1 = []  
+    s2 = []  
+    s1.append(root)
+    while len(s1) != 0:  
+        curr = s1.pop()    
+        for child in curr.children:
+            s1.append(child)
+  
+        if curr.leaf:
+            s2.append(curr)  
+
+    return len(s2)
+
+# printYield(function, [0], "remove")
+pgmLen = getPgmLen(root)
+# print(function.__repr__())
+
+
+#### now we will try to introduce errors in the above syntax tree
+print("")
+# printYield(function, [7])
+pgms =  2
+print("Removal of nodes: ")
+positions = [i for i in range(1,pgmLen)]
+for n_errors in range(1,4):
+    print("Programs with "+str(n_errors)+" errors")
+    for i in range(0,pgms):
+        reqpos = []    
+        for j in range(0,n_errors):
+            c = choice(positions)
+            reqpos.append(c)
+            positions.remove(c)
+        positions = [i for i in range(1,pgmLen)]
+        printYield(root, reqpos, "remove")
+        print("")
+    
+print("Addition of nodes: ")
+positions = [i for i in range(1,pgmLen)]
+for n_errors in range(1,4):
+    print("Programs with "+str(n_errors)+" errors")
+    for i in range(0,pgms):
+        reqpos = []    
+        for j in range(0,n_errors):
+            c = choice(positions)
+            reqpos.append(c)
+            positions.remove(c)
+
+        positions = [i for i in range(1,pgmLen)]
+        printYield(root, reqpos, "add")
+        print("")
+
+print("Replacing of nodes: ")
+positions = [i for i in range(1,pgmLen)]
+for n_errors in range(1,4):
+    print("Programs with "+str(n_errors)+" errors")
+    for i in range(0,pgms):
+        reqpos = []    
+        for j in range(0,n_errors):
+            c = choice(positions)
+            reqpos.append(c)
+            positions.remove(c)
+
+        positions = [i for i in range(1,pgmLen)]
+        printYield(root, reqpos, "replace")
+        print("")
+

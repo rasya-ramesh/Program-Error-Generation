97cc247da3b86e1613bc2e32eeeaa7cfb51c3fa2
ramyani-ghosh <ramyani.ghosh@gmail.com>
2020-03-28 16:55:52 +0530

    Sorted error messages

--- programs/python/toy_programs/output_programs/errors/ArmstrongNumber_1_error.py
+++
@@ -1,5 +0,0 @@
-Line no. 2 :  : missing
-Line no. 3 :  % missing
-Line no. 4 :  + missing
-Line no. 6 :  Unknown % found.
-Line no. 6 :  Unknown . found.
--- programs/python/toy_programs/output_programs/errors/hcf_1_error.py
+++
@@ -1,4 +0,0 @@
-Line no. 2 :  , missing
-Line no. 2 :  Unknown ~ found.
-Line no. 6 :  = missing
-Line no. 7 :  : missing

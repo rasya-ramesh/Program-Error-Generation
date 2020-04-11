7febb96afec2111a6d7276b5f24eed5b9f37184c
ramyani-ghosh <ramyani.ghosh@gmail.com>
2020-04-11 15:07:40 +0530

    Bigger programs

--- programs/c/toy_programs/output_programs/errors/factorial_0_error.c
+++
@@ -1,5 +0,0 @@
-Line no. 4 :  ; missing
-Line no. 5 :  & missing
-Line no. 12 :  } missing
-Line no. 13 :  Unknown : found.
-Line no. 14 :  Unknown [ found.
--- programs/python/toy_programs/output_programs/errors/hcf_1_error.py
+++
@@ -1,5 +0,0 @@
-Line no. 7 :  : missing
-Line no. 8 :  Unknown ^ found.
-Line no. 8 :  Unknown ( found.
-Line no. 9 :  = missing
-Line no. 13 :  ) missing

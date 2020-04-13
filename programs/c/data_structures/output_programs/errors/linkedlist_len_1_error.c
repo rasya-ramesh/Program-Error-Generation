97a10a9703820163540e5472ca658bac9a209609
rasya-ramesh <rasya.ramesh1999@gmail.com>
2020-04-13 23:14:52 +0530

    resolved

--- programs/c/data_structures/output_programs/errors/linkedlist_len_1_error.c
+++
@@ -1,5 +0,0 @@
-Line no. 8 :  ( missing
-Line no. 10 :  Unknown * found.
-Line no. 13 :  ) missing
-Line no. 17 :  Unknown ~ found.
-Line no. 27 :  ; missing
--- programs/c/data_structures/output_programs/errors/stack_1_error.c
+++
@@ -1,5 +0,0 @@
-Line no. 9 :  Unknown ^ found.
-Line no. 11 :  } missing
-Line no. 14 :  * missing
-Line no. 15 :  Unknown |\| found.
-Line no. 20 :  ; missing
